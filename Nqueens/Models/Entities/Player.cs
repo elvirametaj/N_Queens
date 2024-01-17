30421b7a15ef974c3cc1fa8636d0e08446d89364
ErionaM <eriona.mustafa1@student.uni-pr.edu>
2024-01-17 21:55:36 +0100

    Create Player

--- Nqueens/Models/Entities/Player.cs
+++ Nqueens/Models/Entities/Player.cs
@@ -2,5 +2,12 @@
 
 namespace Nqueens.Models.Entities
 {
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
 
+        [Required]
+        public string Username { get; set; }
+    }
 }
