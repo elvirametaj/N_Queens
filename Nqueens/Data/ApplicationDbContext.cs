d0942994e5ecceb5f0a75f7bf305f6d60051577b
ErionaM <eriona.mustafa1@student.uni-pr.edu>
2024-01-17 21:56:45 +0100

    add applicationDbContext

--- Nqueens/Data/ApplicationDbContext.cs
+++ Nqueens/Data/ApplicationDbContext.cs
@@ -3,5 +3,12 @@ using Nqueens.Models.Entities;
 
 namespace Nqueens.Data
 {
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
 
+        public DbSet<Player> Players { get; set; }
+    }
 }
