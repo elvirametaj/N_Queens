7e8b75bd514b4da770d315e0f839bdcc8e33dc6e
eramaliqi19 <era.maliqi2@student.uni-pr.edu>
2024-01-17 13:39:04 +0100

    Added Solution

---
+++ Nqueens/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Nqueens.Models.Entities;
+
+namespace Nqueens.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Player> Players { get; set; }
+    }
+}
---
+++ Nqueens/Models/Entities/Player.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nqueens.Models.Entities
+{
+    public class Player
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+    }
+
+}
