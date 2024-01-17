162dcb5f332e3adb1f679848cd918300c5be7566
eramaliqi19 <era.maliqi2@student.uni-pr.edu>
2024-01-17 13:47:51 +0100

    Fixed issues

--- Nqueens/Data/ApplicationDbContext.cs
+++ Nqueens/Data/ApplicationDbContext.cs
@@ -3,12 +3,5 @@ using Nqueens.Models.Entities;
 
 namespace Nqueens.Data
 {
-    public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions options) : base(options)
-        {
-        }
 
-        public DbSet<Player> Players { get; set; }
-    }
 }
--- Nqueens/Program.cs
+++ Nqueens/Program.cs
@@ -5,9 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PlayersDbConnectionString")));
-
+// krijohet builder ktu
 
 var app = builder.Build();
 
